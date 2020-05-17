16667c4e84ff0775c42303f82367f55da946e8ae
Huy Tran <baohuy251210@gmail.com>
2020-05-17 15:49:43 -0600

    App name provided

--- app.py
+++ app.py
@@ -83,6 +83,7 @@ external_stylesheets = [
 app = dash.Dash(__name__,
                 external_stylesheets=external_stylesheets)
 server = app.server
+app.title = 'Anoroc Monitor'
 
 app.layout = html.Div(id='container', className='parent',
                       children=[
