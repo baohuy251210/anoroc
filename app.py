a48852c0d66015fecee2e658088dd0160afab40d
Huy Tran <baohuy251210@gmail.com>
2020-05-16 20:03:47 -0600

    Beta version ready to start deploy

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import dash_html_components as html
 import pandas as pd
 from datetime import datetime
 import dataprocess
+import dash_table
+from dash.dependencies import Input, Output, State
 '''
 -----------------------
 Data Frame (pandas) to htmlTable
@@ -12,40 +14,64 @@ Data Frame (pandas) to htmlTable
 df = pd.read_csv('jhu_sorted.csv', encoding='cp1252')
 df.columns = ['Country', 'Infected', 'New Infected', 'Total Deaths', 'New Deaths',
               'Total Recovered', 'New Recovered']
+df_search = pd.read_csv('jhu.csv', encoding='cp1252')
+df_search_name_index = pd.read_csv(
+    'jhu.csv', index_col='Country', encoding='cp1252')
+df_search.columns = ['Country', 'Infected', 'New Infected', 'Total Deaths', 'New Deaths',
+                     'Total Recovered', 'New Recovered']
 
 
-def generate_table(dataframe, max_rows=40):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ]),
-    ])
+def generate_dropdown(dframe):
+    lst = []
+    for i in range(len(dframe)):
+        lst.append({'label': dframe.iloc[i][0],
+                    'value': dframe.iloc[i][0]})
+    # print(lst)
+    return dcc.Dropdown(
+        id='country-dropdown',
+        options=lst,
+        placeholder="Select country to inspect",
+        clearable=False,
+        searchable=True,
+        value=df.iloc[0][0],
+        style={
+            'display': 'inline-block',
+            'text-align': 'center',
+            'color': '#000000',
+            'width': '75%',
+            'font-family': 'Roboto',
+        }
+    )
+
+
+def generate_table(dataframe):
+    return html.Table(
+        [
+            html.Thead(
+                html.Tr([html.Th(col) for col in dataframe.columns])
+            ),
+            html.Tbody([
+                html.Tr([
+                    html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+                ]) for i in range(len(dataframe))
+            ]),
+        ], style={
+            'margin-top': '50px',
+        })
 
 
 # def generate_dropdown
 
-'''
+"""
 -----------------------
 App.py layouts part
-'''
+"""
 # external_stylesheets = ['https://codepen.io/baohuy251210/pen/KKdGQep.css']
 external_stylesheets = ['https://codepen.io/baohuy251210/pen/rNOqdKv.css']
 # external_scripts = ['https://codepen.io/zavoloklom/pen/IGkDz.js']
 app = dash.Dash(__name__,
                 external_stylesheets=external_stylesheets)
 
-'''
-layout styling
-'''
-# colors = {
-#     'background': '#fcfffa',
-#     'text': '#4f504e'
-# }
 
 app.layout = html.Div(
     children=[
@@ -59,11 +85,19 @@ app.layout = html.Div(
                 ),
         html.H4(children="Updated Daily (" + datetime.today().strftime('%m-%d-%Y')+")",
                 style={
-                    'textAlign': 'center',
-                    'margin-top': '5px',
+            'textAlign': 'center',
+            'margin-top': '5px',
 
         },
         ),
+        generate_dropdown(df_search),
+        html.Div(id='dropdown-output',
+                 style={
+                     'font-family': 'Roboto',
+                     'width': '75%',
+                     'text-align': 'center',
+                     'display': 'inline-block'
+                 }),
         generate_table(df),
         html.H6(["Data is sourced from ",
                  html.A('John Hopkins CSSE',
@@ -73,12 +107,30 @@ app.layout = html.Div(
                  html.A('COVID19API', href='covid19api.com')
                  ],
                 style={
-                    'font-style': 'italic'
+            'font-style': 'italic'
 
         }
         )
 
     ])
 
+
+@app.callback(
+    Output('dropdown-output', 'children'),
+    [Input('country-dropdown', 'value')])
+def update_output(value):
+    newdf = df_search[df_search['Country'] == value]
+
+    return dash_table.DataTable(
+        id='selected',
+        columns=[{'name': i, 'id': i} for i in newdf.columns],
+        style_cell={
+            'textOverflow': 'ellipsis',
+            'overflow': 'hidden'
+        },
+        data=newdf.to_dict('records')
+    )
+
+
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
